78b32faffb8a07cc28a82f65958ac7fff2538372
Fred Yang <fredyang0507@gmail.com>
2025-05-25 20:53:41 -0400

    Update Vite configuration for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fyang0507.github.io/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
